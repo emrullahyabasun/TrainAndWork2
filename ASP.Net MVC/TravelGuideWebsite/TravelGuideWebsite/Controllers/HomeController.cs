e966ffdfdd2f34b532ea2fc9e6c09e21d8afa7c1
emrullahyabasun <yabasunemrullah@gmail.com>
2024-01-17 18:25:21 +0300

    New Tasks added
    Please note that these are not only tasks that I've worked on during the whole course

---
+++ ASP.Net MVC/TravelGuideWebsite/TravelGuideWebsite/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelGuideWebsite.Entity;
+using TravelGuideWebsite.Models;
+
+namespace TravelGuideWebsite.Controllers
+{
+    [Authorize(Roles ="User,Admin")]
+    public class HomeController : Controller
+    {
+        DataContext db = new DataContext();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+
+
+            User user = Session["User"] as User;
+            if (user != null)
+            {
+                ViewBag.User = user;
+            }
+            else
+            {
+                ViewBag.User = null;
+            }
+
+            var recentPosts = db.BlogPosts.OrderByDescending(p => p.CreationDate).Take(5).ToList();
+            var popularPosts = db.BlogPosts.OrderByDescending(p => p.Likes.Count).Take(5).ToList();
+            ViewBag.RecentPosts = recentPosts;
+            ViewBag.PopularPosts = popularPosts;
+
+            return View();
+        }
+
+
+       
+
+
+
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.Net MVC/TravelGuideWebsite/TravelGuideWebsite/Entity/Process/BlogLikeProcess.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelGuideWebsite.Entity.Interface;
+using TravelGuideWebsite.Models;
+
+namespace TravelGuideWebsite.Entity.Process
+{
+    public class BlogLikeProcess
+    {
+
+        DataContext db = new DataContext();
+        public string Add(int blogPostId, int userId)
+        {
+
+            var existingLike = db.BlogLikes.FirstOrDefault(x => x.BlogPostId == blogPostId && x.UserId == userId);
+            if (existingLike == null)
+            {
+                
+                db.BlogLikes.Add(new BlogLike { BlogPostId = blogPostId, UserId = userId });
+                db.SaveChanges();
+                return "Beğenildi";
+            }
+            else
+            {
+                
+                existingLike.IsDeleted = !existingLike.IsDeleted;
+                db.SaveChanges();
+                return existingLike.IsDeleted ? "Beğeni kaldırıldı" : "Beğenildi";
+            }
+        }
+
+        public int LikeCount(int blogPostId)
+        {
+            return db.BlogLikes.Count(x => x.BlogPostId == blogPostId && !x.IsDeleted);
+        }
+
+        
+    }
+}
